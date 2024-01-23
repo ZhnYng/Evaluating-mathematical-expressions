773107068d13a95e582ed89079fb8b44e0dd0362
Zhen Yang <zzhenyyang@gmail.com>
2024-01-23 09:49:20 +0800

    Exit submenu with cntrl+c

--- main.py
+++ main.py
@@ -8,22 +8,22 @@ interface = Interface()
 options = Options()
 
 # Define a function to start the program
-def start_program():
+choice = ''
+def start_program(choice):
     # Display the program banner
     interface.banner()
     #Main program loop
-    choice = ''
-    while choice != '8':
-        # Prompt the user for their choice
-        choice = input("Please select your choice: ('1','2','3','4','5','6'):\n"
-                       '    1. Add/Modify assignment statement\n'
-                       '    2. Display current assignment statements\n'
-                       '    3. Evaluate a single variable\n'
-                       '    4. Read assignment statements from file\n'
-                       '    5. Sort assignment statements\n'
-                       '    6. Exit\n'
-                       'Enter choice: ')
+    while choice != '6':
         try:
+            # Prompt the user for their choice
+            choice = input("Please select your choice: ('1','2','3','4','5','6'):\n"
+                        '    1. Add/Modify assignment statement\n'
+                        '    2. Display current assignment statements\n'
+                        '    3. Evaluate a single variable\n'
+                        '    4. Read assignment statements from file\n'
+                        '    5. Sort assignment statements\n'
+                        '    6. Exit\n'
+                        'Enter choice: ')
             # Use a match statement to handle user choices
             match choice:
                 case '1':
@@ -42,15 +42,52 @@ def start_program():
                     interface.option5(options.sorting_expressions)
                     pass
                 case '6':
-                    print("\nBye, thanks for using ST1507 DSAA: Assignment Statement Evaluator & Sorter!")
+                    print('\nBye, thanks for using ST1507 DSAA: Caesar Cipher Encrypted Message Analyzer')
                     break
         except Exception as e:
             # Handle exceptions and display an error message
             print(f'\n{e}')
-    else:
-        # Display a goodbye message when the program exits
-        print('\nBye, thanks for using ST1507 DSAA: Caesar Cipher Encrypted Message Analyzer')
+        except KeyboardInterrupt:
+            # Handle exit of sub menu
+            # Display the program banner
+            print('\n')
+            interface.banner()
+            try:
+                # Prompt the user for their choice
+                choice = input("Please select your choice: ('1','2','3','4','5','6'):\n"
+                            '    1. Add/Modify assignment statement\n'
+                            '    2. Display current assignment statements\n'
+                            '    3. Evaluate a single variable\n'
+                            '    4. Read assignment statements from file\n'
+                            '    5. Sort assignment statements\n'
+                            '    6. Exit\n'
+                            'Enter choice: ')
+                # Use a match statement to handle user choices
+                match choice:
+                    case '1':
+                        interface.option1(options.add_or_modify)
+                        pass
+                    case '2':
+                        interface.option2(options.display_statements)
+                        pass
+                    case '3':
+                        interface.option3(options.eval_one_var)
+                        pass
+                    case '4':
+                        interface.option4(options.read_from_file)
+                        pass
+                    case '5':
+                        interface.option5(options.sorting_expressions)
+                        pass
+                    case '6':
+                        print('\nBye, thanks for using ST1507 DSAA: Caesar Cipher Encrypted Message Analyzer')
+                        break
+            except Exception as e:
+                # Handle exceptions and display an error message
+                print(f'\n{e}')
+            except KeyboardInterrupt:
+                print("\n\nPress '6' to exit!")
 
 # Entry point of the program
 if __name__ == '__main__':
-    start_program()
+    start_program(choice)
